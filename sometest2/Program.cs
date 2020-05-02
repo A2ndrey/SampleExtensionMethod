27c7b66c56e541333bdac76c67ae6285cf8db353
AnDreW <andrew21ls@yandex.ru>
2020-05-02 14:10:32 +0300

    add one sample

--- sometest2/Program.cs
+++ sometest2/Program.cs
@@ -10,12 +10,29 @@ namespace sometest2
         }
     }
 
+    static class MeRandomFromMinToMax
+    {
+        public static double NextDouble(this Random rnd, double min, double max)
+        {
+            return rnd.NextDouble() * (max-min) + min;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             var arg1 = "100500";
             Console.Write(arg1.ToInt() + "42".ToInt()); // 100542
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Default random:");
+            var rnd = new Random();
+            Console.WriteLine(rnd.NextDouble());
+
+            Console.WriteLine("My random form 3 to 9:");
+            Console.WriteLine(rnd.NextDouble(3, 9));
         }
     }
 }
