86f4531a874a6ebef2ab88ebabbaa18c4612258b
AnDreW <andrew21ls@yandex.ru>
2020-05-02 14:00:46 +0300

    Добавьте файлы проекта.

---
+++ sometest2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sometest2
+{
+    static class MyConvert
+    {
+        public static int ToInt(this string a)
+        {
+            return Convert.ToInt32(a);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arg1 = "100500";
+            Console.Write(arg1.ToInt() + "42".ToInt()); // 100542
+        }
+    }
+}
